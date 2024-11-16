5cb905184c6813e7819d5bcd121684bc8bba444c
coconutturtle <palani3124@gmail.com>
2024-11-16 02:37:17 +0000

    update

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// Initialize AOS (Animate on Scroll)
+document.addEventListener('DOMContentLoaded', function () {
+  AOS.init({
+    duration: 1000, // Animation duration in milliseconds
+    once: true // Trigger animation only once
+  });
+});
--- scripts.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    AOS.init({
-      duration: 1000, // Animation duration in milliseconds
-      once: true // Run animation only once
-    });
-  });
-  
