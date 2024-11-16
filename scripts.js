02e54f133f4c236f9f52ed9f68e54a6f68b54b4d
coconutturtle <palani3124@gmail.com>
2024-11-16 02:29:18 +0000

    skyview

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    AOS.init({
+      duration: 1000, // Animation duration in milliseconds
+      once: true // Run animation only once
+    });
+  });
+  
